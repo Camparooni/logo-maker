df586c69de94b6ecf3fe1e2b1d7e4db0c93e651b
Tay Campanelli <tayascamp@yahoo.com>
2023-10-22 17:00:52 -0700

    updated prompts and added character limit for logo text

--- index.js
+++ index.js
@@ -4,36 +4,45 @@ const fs = require('fs');
 
 const Shape = require('./lib/shapes');
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name:'shape',
-            message:'What shape is your logo?',
-            choices: ['square','circle','triangle'],
-        },
-        {
-            type: 'input',
-            name:'title',
-            message:'What name would you like inside your logo?',
-        },
-        {
-            type: 'input',
-            name:'titleColor',
-            message:'What color is your title?',
-        },
-        {
-            type: 'input',
-            name:'shapeColor',
-            message:'What color is your logo shape?',
-        }
-    ])
+function promptUser() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name:'shape',
+                message:'What shape would you like your logo to be?',
+                choices: ['square','circle','triangle'],
+            },
+            {
+                type: 'input',
+                name:'title',
+                message:'What text would you like displayed in your logo? (up to three characters)',
+            },
+            {
+                type: 'input',
+                name:'titleColor',
+                message:'Choose your logos text color',
+            },
+            {
+                type: 'input',
+                name:'shapeColor',
+                message:'Choose your background color',
+            }
+        ])
+
     .then((answers) => {
+        if (answers.title.length > 3) {
+            console.log('Display text must me no more than three characters');
+            promptUser();
+        } else {
         console.log(answers);
         const svg = new Shape(answers.shape, answers.title, answers.titleColor, answers.shapeColor);
         // const htmlPageContent = generateHTML(answers);
     
-        fs.writeFile('shape.svg', svg.render(), (err) =>
+        fs.writeFile('logo.svg', svg.render(), (err) =>
             err ? console.log(err) : console.log('Successfully created index.html!')
         );
-    });
+    }});
+}
+
+promptUser();
